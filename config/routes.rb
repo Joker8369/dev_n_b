d900c0dd1cfb4d8ef9f97ada5b0d20e4777e873f
Gr-michael <grosheny.michael@gmail.com>
2021-08-18 13:59:21 +0200

    seed user, user page et bouton log_in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :user, only: :show
+  resources :users, only: :show
   resources :developers do
     resources :bookings, only: %i[new create index destroy]
   end
